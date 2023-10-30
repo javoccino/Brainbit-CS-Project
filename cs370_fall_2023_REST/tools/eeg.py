189bacbb94d6fbb724a4a8a0f421f3f683e0d524
Juulio-p <pjulio1234@gmail.com>
2023-10-29 17:05:39 -0700

    Made the stop_Movie call and Implemented it into The Javascript file index

--- cs370_fall_2023_REST/tools/eeg.py
+++ cs370_fall_2023_REST/tools/eeg.py
@@ -5,14 +5,30 @@ from neurosdk.cmn_types import *
 
 from tools.logging import logger   
 
-
+import pickle
 #doing all this a the "module level" in "Demo" server mode it will work fine :)
+
+#user_data {
+ #   username : "me"
+  #  movie1 = []
+#}
+
 
 def on_sensor_state_changed(sensor, state):
     logger.debug('Sensor {0} is {1}'.format(sensor.Name, state))
 
 def on_brain_bit_signal_data_received(sensor, data):
+    
     logger.debug(data)
+   # with open('data.pkl' , 'wb') as f:
+       # pickle.dump(data . f)
+        #f.close()
+
+    #with open('data.pkl' , 'rb') as f:
+        #dataLoaded = pickle.load(f)
+        #print("Pickled" + dataLoaded) 
+    #    user_data[movie1.append(data)]
+
 
 logger.debug("Create Headband Scanner")
 gl_scanner = Scanner([SensorFamily.SensorLEBrainBit])
@@ -24,6 +40,7 @@ def sensorFound(scanner, sensors):
     for i in range(len(sensors)):
         logger.debug('Sensor %s' % sensors[i])
         logger.debug('Connecting to sensor')
+
         gl_sensor = gl_scanner.create_sensor(sensors[i])
         gl_sensor.sensorStateChanged = on_sensor_state_changed
         gl_sensor.connect()
