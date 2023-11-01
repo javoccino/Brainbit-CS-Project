68f6dc3e181510d993fe49030e254e6be510d387
jktckr <59899897+jktckr@users.noreply.github.com>
2023-11-01 16:21:00 -0700

    jose index with stop working

--- cs370_fall_2023_REST/open_calls/stop_movie.py
+++ cs370_fall_2023_REST/open_calls/stop_movie.py
@@ -10,13 +10,15 @@ def handle_request():
 
     g.hb.exec_command(SensorCommand.CommandStopSignal)
 
-    #with open("%s_data.pkl" % UserInfo["Username"], 'wb') as f:  # open pkl file
-    #    pickle.dump(UserInfo, f) # serialize the list
-    #    f.close()
+    with open("%s_data.pkl" % UserInfo["Username"], 'wb') as f:  # open pkl file
+        pickle.dump(UserInfo, f) # serialize the list
+        f.close()
 
-    #with open("%s_data.pkl" % UserInfo["Username"], 'rb') as f:  #'testing_data_objects.pkl'
-    #    one_data = pickle.load(f) # deserialize using load()
-    #    print(one_data) # print student names
+    print("\n-------------Printing plk-----------------------\n") #to see if the pkl works
+
+    with open("%s_data.pkl" % UserInfo["Username"], 'rb') as f:  #'testing_data_objects.pkl'
+        one_data = pickle.load(f) # deserialize using load()
+        print(one_data) # print student names
 
 
     return ["Data Stop Flowing"]
