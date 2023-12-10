9411f8568b8ef4742f1ff0db641d167f6c84fb31
jktckr <59899897+jktckr@users.noreply.github.com>
2023-12-09 18:49:53 -0800

    Moved pickle store from signup to stop_movie.py

--- cs370_fall_2023_REST/open_calls/stop_movie.py
+++ cs370_fall_2023_REST/open_calls/stop_movie.py
@@ -1,10 +1,24 @@
 from flask import request, g                                                                 
 from tools.logging import logger   
 from neurosdk.cmn_types import * 
+from tools.eeg import UserInfo
+import pickle
 
 def handle_request():
     if g.hb == None:
         return ["Data Flowing"]
 
     g.hb.exec_command(SensorCommand.CommandStopSignal)
+
+    print("\n-------------Printing plk-----------------------\n") #to see if the pkl works
+
+    with open("%s_data.pkl" % UserInfo["Username"], 'wb') as f:  #'testing_data_objects.pkl'
+        pickle.dump(UserInfo, f) # serialize the list
+        f.close()
+        
+    
+    print("\n-------------finish plk-----------------------\n")
+
     return ["Data Stop Flowing"]
+
+  
